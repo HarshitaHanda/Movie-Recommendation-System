65fb698ece676a2eba8c8594a97e8a25bc9a2375
HarshitaHanda <149144740+HarshitaHanda@users.noreply.github.com>
2024-09-18 02:56:55 +0530

    app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import py7zr
+import os
+
+# Extract similarity.7z to get similarity.pkl if not already extracted
+if not os.path.exists('similarity.pkl'):
+    with py7zr.SevenZipFile('similarity.7z', mode='r') as z:
+        z.extractall()
 
 def fetch_poster(movie_id):
-    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=64260b9462a7b1ab55ed019e24d26699'.format(movie_id))
-    data=response.json()
-    return  "https://image.tmdb.org/t/p/w500" + data['poster_path']
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=64260b9462a7b1ab55ed019e24d26699'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
 
 # Load the movie list and similarity matrix
 movie_list_df = pickle.load(open('movies.pkl', 'rb'))
@@ -19,13 +26,17 @@ def recommend(movie):
     movie_index = movie_list_df[movie_list_df['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
     recommended_movies = []
-    recommended_movies_poster=[]
+    recommended_movies_poster = []
+    
     for i in movie_list:
-        movie_id=movie_list_df.iloc[i[0]].id
+        movie_id = movie_list_df.iloc[i[0]].id
         recommended_movies.append(movie_list_df.iloc[i[0]].title)
         recommended_movies_poster.append(fetch_poster(movie_id))
+    
     return recommended_movies, recommended_movies_poster
+
 # Streamlit UI
 st.title('Movie Recommendation System')
 
@@ -34,8 +45,9 @@ option = st.selectbox(
     movie_titles)
 
 if st.button('Recommend'):
-    names,posters = recommend(option)
+    names, posters = recommend(option)
     st.write("Recommended Movies:")
+    
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
@@ -49,6 +61,7 @@ if st.button('Recommend'):
     with col3:
         st.text(names[2])
         st.image(posters[2])
+
     with col4:
         st.text(names[3])
         st.image(posters[3])
